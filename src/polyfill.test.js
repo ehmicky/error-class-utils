69fc59433725f2c8105e23ce2559deafaf0a4130
ehmicky <ehmicky@gmail.com>
2024-02-11 13:44:46 +0000

    Fix linting

--- src/polyfill.test.js
+++ src/polyfill.test.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line n/file-extension-in-import, import/no-unassigned-import
+// eslint-disable-next-line import/no-unassigned-import
 import 'error-cause-polyfill/auto'
 
 // eslint-disable-next-line import/no-unassigned-import
